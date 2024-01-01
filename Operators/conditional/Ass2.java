36a9da77835d5ab75b6c50a3b0adbf65bc5c955f
imadiwalke16 <walkeaditya3@gmail.com>
2024-01-01 23:12:32 +0530

    operators_assingments 3

---
+++ Operators/conditional/Ass2.java
@@ -0,0 +1,74 @@
+package Operators.conditional;
+import java.util.*;
+public class Ass2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter your number 1");
+        // int num_a=sc.nextInt();
+        // System.out.println("Enter your no. 2");
+        // int num_b=sc.nextInt();
+        // if(num_a>0 && num_b>0){
+        //     System.out.println("your number is positive");
+
+        // }
+        // else{
+        //     System.out.println("your number is negative");
+
+        // }
+        // int day=sc.nextInt();
+        // switch (day) {
+        //     case 1:
+        //         System.out.println("Your day is Monday");
+        //         break;
+        
+        //     case 2:
+        //         System.out.println("Your day is Tuesday");
+        //         break;
+        
+        //     case 3:
+        //         System.out.println("Your day is Wednesday");
+        //         break;
+        
+        //     case 4:
+        //         System.out.println("Your day is Thursday");
+        //         break;
+        
+        //     case 5:
+        //         System.out.println("Your day is Friday");
+        //         break;
+        
+        //     case 6:
+        //         System.out.println("Your day is Saturday");
+        //         break;
+        
+        //     case 7:
+        //         System.out.println("Your day is Sunday");
+        //         break;
+        
+        //     default:
+        //     System.out.println("invalid choice");
+        //         break;
+        // // }
+        // int a=63;
+        // int b=36;
+        // boolean x=(a<b)? true:false;
+        // int y=(a>b)?a:b;
+        // System.out.println(y);
+        // System.out.println("Enter Year");
+        // int year=sc.nextInt();
+
+        // boolean x=(year%4)==0; 
+        // boolean y=(year%100)!=0;
+        // boolean z=((year%100==0)&&(year % 400==0));
+        // if(x&&(y||z)){
+        //     System.out.println("leap year");
+
+        // }
+        // else{
+        //     System.out.println("not a leap year");
+        // }
+        }
+
+    }
+    
+
